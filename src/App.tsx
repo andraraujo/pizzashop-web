992fe470ea3ea6cedf43f507c195c2c1ce927fec
Andressa R Araujo <andaraujo1998@gmail.com>
2024-04-25 11:48:01 -0300

    feat: configuração inicial de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
+import { RouterProvider } from "react-router-dom";
 import "./global.css";
-import { Button } from "@/components/ui/button";
+import { router } from "./routes";
 
 export function App() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>Button</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
