9dd615cfa6f66f9307df44fa5da16a9b59497084
Andressa R Araujo <andaraujo1998@gmail.com>
2024-04-26 22:27:01 -0300

    feat: adicionando o toaster

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import "./global.css";
 import { router } from "./routes";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Toaster } from "sonner";
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   );
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -20,9 +21,21 @@ export function SignIn() {
   } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
+    try {
+      console.log(data);
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos um link de autenticação para o seu e-mail.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => {
+            handleSignIn(data);
+          },
+        },
+      });
+    } catch {
+      toast.error("Credenciais inválidas");
+    }
   }
 
   return (
