774bde8f91aa4903b352c195a3195be39930cac7
Andressa R Araujo <andaraujo1998@gmail.com>
2024-04-29 16:25:13 -0300

    feat: criação inicial da tela de listagem de pedidos

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ export const router = createBrowserRouter([
       {
         path: "/sign-up",
         element: <SignUp />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
       },
     ],
   },
