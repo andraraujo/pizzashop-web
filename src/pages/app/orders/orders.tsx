777bffe1d6125629ffe50abe92bed87ab3decc5c
Andressa R Araujo <andaraujo1998@gmail.com>
2024-04-29 16:48:05 -0300

    feat: adicionando paginação

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -43,6 +43,28 @@ export function Orders() {
             </TableBody>
           </Table>
         </div>
+
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return <OrderTableRow key={i} />;
+              })}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </>
   );
